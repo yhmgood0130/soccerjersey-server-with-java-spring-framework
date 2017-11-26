f7928920e4d5b8f5f1b20cd213187e3e2e72ca4c
Hyunmo Yang <yhmgood0130@gmail.com>
2017-11-25 22:52:29 -0700

    Upload-Image Repo added

--- src/main/java/com/soccerjerseystore/resource/JerseyResource.java
+++ src/main/java/com/soccerjerseystore/resource/JerseyResource.java
@@ -1,10 +1,23 @@
 package com.soccerjerseystore.resource;
 
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.util.Iterator;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 import com.soccerjerseystore.domain.Jersey;
 import com.soccerjerseystore.service.JerseyService;
@@ -12,11 +25,36 @@ import com.soccerjerseystore.service.JerseyService;
 @RestController
 @RequestMapping("/jersey")
 public class JerseyResource {
+	
 	@Autowired
 	private JerseyService jerseyService; 
 	
 	@RequestMapping(value="/add", method=RequestMethod.POST)
 	public Jersey addJerseyPost(@RequestBody Jersey jersey) {
 		return jerseyService.save(jersey);
+	}
+	
+	@RequestMapping(value="/add/image", method=RequestMethod.POST)
+	public ResponseEntity upload(
+			@RequestParam("id") Long id,
+			HttpServletResponse response, HttpServletRequest request
+			) {
+		try {
+			Jersey jersey = jerseyService.findOne(id);
+			MultipartHttpServletRequest multipartRequest = (MultipartHttpServletRequest) request;
+			Iterator<String> it = multipartRequest.getFileNames();
+			MultipartFile multipartFile = multipartRequest.getFile(it.next());
+			String fileName = id+".png";
+			
+			byte[] bytes = multipartFile. getBytes();
+			BufferedOutputStream stream = new BufferedOutputStream(new FileOutputStream(new File("src/main/resources/static/image/jersey/" + fileName)));
+			stream.write(bytes);
+			stream.close();
+			
+			return new ResponseEntity("Upload Success!!", HttpStatus.OK);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return new ResponseEntity("Upload failed!", HttpStatus.BAD_REQUEST);
+		}
 	}
 }
